17396aaa774d6ae1dea6c74b2e1bfa6eea1dfde9
muhamadrakif <muhamad.rakif.p@gmail.com>
2022-06-28 01:15:09 +0700

    ubah dashboard

--- hal/dashboard/dashboard.php
+++ hal/dashboard/dashboard.php
@@ -1,5 +1,5 @@
      <h2>Admin Dashboard</h2>   
-     <h5>Welcome Jhon Deo , Love to see you back. </h5>
+     <h5>Welcome, Love to see you back. </h5>
 
      <!-- /. ROW  -->
      <hr />
@@ -10,7 +10,16 @@
      				<i class="fa fa-envelope-o"></i>
      			</span>
      			<div class="text-box" >
-     				<p class="main-text">120</p>
+     				<p class="main-text">
+               <?php
+
+                $sql = $koneksi->  query ("SELECT COUNT(jumlah_buku) FROM tb_buku");
+                while ($data=$sql-> fetch_assoc()){
+                  echo $data['COUNT(jumlah_buku)'];
+                  }
+                ?> 
+
+               </p>
      				<p class="text-muted">Jumlah Buku</p>
      			</div>
      		</div>
@@ -21,7 +30,16 @@
      				<i class="fa fa-bars"></i>
      			</span>
      			<div class="text-box" >
-     				<p class="main-text">30</p>
+     				<p class="main-text">
+                  <?php
+
+                $sql = $koneksi->  query ("SELECT COUNT(nim) FROM tb_anggota;");
+                while ($data=$sql-> fetch_assoc()){
+                  echo $data['COUNT(nim)'];
+                  }
+                ?> 
+
+               </p>
      				<p class="text-muted">Anggota</p>
      			</div>
      		</div>
@@ -32,7 +50,16 @@
      				<i class="fa fa-bell-o"></i>
      			</span>
      			<div class="text-box" >
-     				<p class="main-text">240</p>
+     				<p class="main-text">
+            
+               <?php
+
+                $sql = $koneksi->  query ("SELECT COUNT(nama) AS total FROM tb_transaksi WHERE status = 'pinjam';");
+                while ($data=$sql-> fetch_assoc()){
+                  echo $data['total'];
+                  }
+                ?>       
+               </p>
      				<p class="text-muted">Peminjam</p>
      			</div>
      		</div>
